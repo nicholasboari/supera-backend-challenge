282f3e8245c6359d707def0e46ab8803debc919f
nicholasboari <nicholasboari@gmail.com>
2023-07-19 18:59:14 -0300

    feat: Spring Doc

--- src/main/java/br/com/banco/controller/AccountController.java
+++ src/main/java/br/com/banco/controller/AccountController.java
@@ -2,6 +2,7 @@ package br.com.banco.controller;
 
 import br.com.banco.dto.AccountDTO;
 import br.com.banco.service.AccountService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,30 +17,35 @@ public class AccountController {
 
     private final AccountService accountService;
 
+    @Operation(summary = "return all accounts in a page")
     @GetMapping("/all")
     public ResponseEntity<Page<AccountDTO>> findAll(Pageable pageable) {
         Page<AccountDTO> page = accountService.findAll(pageable);
         return ResponseEntity.ok().body(page);
     }
 
+    @Operation(summary = "return account in a page by id")
     @GetMapping("/{id}")
     public ResponseEntity<AccountDTO> findById(@PathVariable Long id) {
         AccountDTO account = accountService.findById(id);
         return ResponseEntity.ok().body(account);
     }
 
+    @Operation(summary = "create an account")
     @PostMapping
     public ResponseEntity<AccountDTO> save(@RequestBody AccountDTO accountDTO) {
         AccountDTO account = accountService.save(accountDTO);
         return ResponseEntity.status(HttpStatus.CREATED.value()).body(account);
     }
 
+    @Operation(summary = "update an account by id")
     @PutMapping("/{id}")
     public ResponseEntity<AccountDTO> save(@RequestBody AccountDTO accountDTO, @PathVariable Long id) {
         AccountDTO account = accountService.update(accountDTO, id);
         return ResponseEntity.status(HttpStatus.CREATED.value()).body(account);
     }
 
+    @Operation(summary = "delete an account by id")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> save(@PathVariable Long id) {
         accountService.delete(id);
--- src/main/java/br/com/banco/controller/TransferController.java
+++ src/main/java/br/com/banco/controller/TransferController.java
@@ -2,7 +2,9 @@ package br.com.banco.controller;
 
 import br.com.banco.dto.TransferDTO;
 import br.com.banco.service.TransferService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -16,33 +18,37 @@ public class TransferController {
 
     private final TransferService transferService;
 
+    @Operation(summary = "return all transfers in a page")
     @GetMapping("/all")
-    public ResponseEntity<Page<TransferDTO>> findAll(Pageable pageable) {
+    public ResponseEntity<Page<TransferDTO>> findAll(@ParameterObject Pageable pageable) {
         Page<TransferDTO> list = transferService.findAll(pageable);
         return ResponseEntity.ok().body(list);
     }
 
+    @Operation(summary = "return transfers in a page by date")
     @GetMapping
     public ResponseEntity<Page<TransferDTO>> findByDate(
             @RequestParam("startDate") String startDate,
             @RequestParam("endDate") String endDate,
-            Pageable pageable) {
+            @ParameterObject Pageable pageable) {
         Page<TransferDTO> page = transferService.findByDateBetween(startDate, endDate, pageable);
         return ResponseEntity.ok().body(page);
     }
 
+    @Operation(summary = "return transfers in a page by Operator Name")
     @GetMapping("/find")
-    public ResponseEntity<Page<TransferDTO>> findByOperatorName(@RequestParam(name = "operatorName") String name, Pageable pageable) {
+    public ResponseEntity<Page<TransferDTO>> findByOperatorName(@RequestParam(name = "operatorName") String name, @ParameterObject Pageable pageable) {
         Page<TransferDTO> page = transferService.findByOperatorName(name, pageable);
         return ResponseEntity.ok().body(page);
     }
 
+    @Operation(summary = "return transfers in a page by Operator Name and Date")
     @GetMapping("/{operatorName}")
     public ResponseEntity<Page<TransferDTO>> findByOperatorNameAndDateBetween(
             @PathVariable String operatorName,
             @RequestParam("startDate") String startDate,
             @RequestParam("endDate") String endDate,
-            Pageable pageable) {
+            @ParameterObject Pageable pageable) {
         Page<TransferDTO> page = transferService.findByOperatorNameAndDateBetween(operatorName, startDate, endDate, pageable);
         return ResponseEntity.ok().body(page);
     }
